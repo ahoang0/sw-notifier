d77741972940f405455a706ae1a760731c85b996
ahoang0 <angie.hoang@chicagobooth.edu>
2019-11-23 20:42:07 +0000

    user_account

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_23_202743) do
+ActiveRecord::Schema.define(version: 2019_11_23_203815) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 2019_11_23_202743) do
     t.integer "user_id"
     t.boolean "fifteen_minute_reminder_sent", default: false
     t.boolean "five_minute_reminder_sent", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
